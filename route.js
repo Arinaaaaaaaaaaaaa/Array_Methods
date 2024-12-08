3e614f120e3afcf927d5f3197c93f1fcba82a209
Arina <horbunovaarina@gmail.com>
2024-12-08 12:17:10 +0200

    .

---
+++ route.js
@@ -0,0 +1,20 @@
+let route = [];
+route.push("Індустріальна");
+alert(route)
+route.push("Тракторний завод");
+alert(route)
+route.push("ім. Масельського");
+alert(route)
+route.push("Армійська");
+alert(route)
+route.push("Палац спорту");
+alert(route)
+route.push("Турбоатом");
+alert(route)
+route.push("Завод ім. Малишева");
+alert(route);
+while(route.length > 0){
+    alert(route.pop());
+alert(route)
+}
+alert("Ви вже проїхали всі станції")
